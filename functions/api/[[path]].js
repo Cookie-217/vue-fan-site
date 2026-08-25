8b46d9b38b79f9f8ff2a3177125d37c40e4f3f02
Cookie-217 <3236219746@qq.com>
2026-08-25 18:30:56 +0800

    test function

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,12 +1,3 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url)
-  const target = 'https://syq-fans-backend-production.up.railway.app' + url.pathname + url.search
-  const headers = new Headers(context.request.headers)
-  headers.delete('host')
-  const init = {
-    method: context.request.method,
-    headers: headers,
-    body: ['GET', 'HEAD'].includes(context.request.method) ? undefined : context.request.body
-  }
-  return fetch(target, init)
+export async function onRequest() {
+  return new Response('HELLO-FUNCTION-OK')
 }
