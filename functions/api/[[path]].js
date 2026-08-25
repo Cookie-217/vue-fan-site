be043fc0d08a68b5ee6d1825fdc16a858adf50f4
Cookie-217 <3236219746@qq.com>
2026-08-25 19:20:51 +0800

    forward content-type header

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,9 +1,10 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url)
   const target = 'https://syq-fans-backend-production.up.railway.app' + url.pathname + url.search
-  const headers = {}
-  const token = context.request.headers.get('token')
-  if (token) headers['token'] = token
+  const headers = new Headers(context.request.headers)
+  headers.delete('host')
+  headers.delete('content-length')
+  headers.delete('connection')
   const init = {
     method: context.request.method,
     headers: headers,
