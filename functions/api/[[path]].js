790c14fccfd85631444d24bec5816337ac697b43
Cookie-217 <3236219746@qq.com>
2026-08-25 18:36:09 +0800

    remove api redirect, use function only

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,3 +1,13 @@
-export async function onRequest() {
-  return new Response('HELLO-FUNCTION-OK')
+export async function onRequest(context) {
+  const url = new URL(context.request.url)
+  const target = 'https://syq-fans-backend-production.up.railway.app' + url.pathname + url.search
+  const headers = {}
+  const token = context.request.headers.get('token')
+  if (token) headers['token'] = token
+  const init = {
+    method: context.request.method,
+    headers: headers,
+    body: ['GET', 'HEAD'].includes(context.request.method) ? undefined : context.request.body
+  }
+  return fetch(target, init)
 }
